91e814e1d57c05e70a4e2427aec7e5436f2d7150
Pedro Braga <83465724+Pedro-H-Braga@users.noreply.github.com>
2022-11-30 19:47:43 -0300

    Atualização script01

--- Lista 03/script01.py
+++ Lista 03/script01.py
@@ -5,16 +5,15 @@ Fazer um programa que solicite ao usuário um valor inteiro positivo e gere o se
 O QUE TENHO QUE FAZER? 
 - minha saída tem que ser o binário daquele número 
 - fatorar o numero e pegar a quantidade de vezes que ele será dividido para pegar o resto
-- a fatoração em binario ocorre no range de 2 números
+- a fatoração em binario ocorre no range de números até 2
 - pegar o resto do numero...
-
 '''
-
-while positivo > 0:
-    x = int(input("Informe um valor inteiro: "))
-    # se numero recebido negativo, trate..
-    if contador > 0:
-        positivo == True
-        
-        
-    else: positivo == False
+a = True
+while a == True:
+    entrada = int(input("Informe um valor inteiro: "))
+    # receber numero int positivo
+    if entrada > 0:
+        n_bin = bin(entrada)
+        print(f"({n_bin}) e o binario de ({entrada})")
+# vou precisar fazer um laço que calcule o resto desse numero até ele ser maior que 2
+# vou precisar de um (operador auxiliar que guardará os restos das divisões de forma que vá somando os seus resto) e um (operador que guardará a fatoração do numero até ele ser maior que 2)
