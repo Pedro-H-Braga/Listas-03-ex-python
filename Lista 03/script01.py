61e8e5657da5cee4e8ed9bb81ff10f6b4fe07151
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-11-29 21:55:55 -0300

    documentando projeto

---
+++ Lista 03/script01.py
@@ -0,0 +1,20 @@
+'''
+Fazer um programa que solicite ao usuário um valor inteiro positivo e gere o seu valor em binário
+--ATENÇÃO--: Deverá ser utilizada divisões inteiras para a solução
+
+O QUE TENHO QUE FAZER? 
+- minha saída tem que ser o binário daquele número 
+- fatorar o numero e pegar a quantidade de vezes que ele será dividido para pegar o resto
+- a fatoração em binario ocorre no range de 2 números
+- pegar o resto do numero...
+
+'''
+
+while positivo > 0:
+    x = int(input("Informe um valor inteiro: "))
+    # se numero recebido negativo, trate..
+    if contador > 0:
+        positivo == True
+        
+        
+    else: positivo == False
